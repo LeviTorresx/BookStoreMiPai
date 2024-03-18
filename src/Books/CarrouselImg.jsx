28af24f597d4416d1b9c0f3640e8601ae2dce951
Davidson Perez <trosquinalidad132@gmail.com>
2024-03-17 23:55:08 -0500

    carrusel

--- src/Books/CarrouselImg.jsx
+++ src/Books/CarrouselImg.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import ImageGallery from "react-image-gallery"
+import "react-image-gallery/styles/css/image-gallery.css"
 
 export default function CarrouselImg() {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/600/800/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/600/800/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/600/800/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
   return (
     <div>
-      
+      <div style={{width: "30%", margin: "auto"}}>
+        <ImageGallery items = {images}
+        showPlayButton = {false}
+        showFullscreenButton = {false}
+        showThumbnails = {false}
+        />
+      </div>
     </div>
   );
 }
