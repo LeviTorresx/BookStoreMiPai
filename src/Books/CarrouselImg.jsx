972c57e6c23c372968b68d9abea3be545f266ce4
Davidson Perez <trosquinalidad132@gmail.com>
2024-03-18 00:06:03 -0500

    config adicional del carrusel

--- src/Books/CarrouselImg.jsx
+++ src/Books/CarrouselImg.jsx
@@ -25,6 +25,8 @@ export default function CarrouselImg() {
         showPlayButton = {false}
         showFullscreenButton = {false}
         showThumbnails = {false}
+        showBullets = {true}
+        autoPlay = {true}
         />
       </div>
     </div>
